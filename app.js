df5f4c91f69ced63c107b5781742770633665fcf
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-10-22 22:52:42 +0530

    Using new operator

--- app.js
+++ app.js
@@ -20,4 +20,11 @@ function PersonMake(name, age) {
 
 }
 
+PersonMake.prototype.talk = function(){
+    console.log(`Hi, My name is ${this.name}`);
+    
+}
+
 let p1 = new PersonMake("subhamoy", 25)
+// new operator = if we already created a constructor then by using new operator we can create a new object or instance
+console.log(p1);
