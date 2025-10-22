036a9ef61ae10d262437d0136ee62df784f762da
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-10-22 22:42:21 +0530

    Using constructor

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-function personMAker(name, age) {
-  const person = {
-    name: name,
-    age: age,
-    talk() {
-      console.log(`Hello, I'm ${this.name}`);
-    },
-  };
+// function personMAker(name, age) {
+//     // personMaker is a factory function(a function that create objects) which returns onject
+//   const person = {
+//     name: name,
+//     age: age,
+//     talk() {
+//       console.log(`Hello, I'm ${this.name}`);
+//     },
+//   };
 
-  return person;
+//   return person;
+// }
+function PersonMake(name, age) {
+    // this is better approach than function maker
+  // PersonMake is a constructor
+  // a constructor doesn't return any value and it's name start with capital letter
+
+  this.name = name, 
+  this.age = age;
+
 }
+
+let p1 = new PersonMake("subhamoy", 25)
